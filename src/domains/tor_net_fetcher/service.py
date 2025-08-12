4f7f2e25349933a08d298f4b31223d540de76fa6
Shireen Anees Ahmed <shireenbanu89@gmail.com>
2025-08-12 14:05:53 -0700

    Completed the integration of Cloudfare api and saving it to db

--- src/domains/tor_net_fetcher/service.py
+++ src/domains/tor_net_fetcher/service.py
@@ -5,13 +5,16 @@ from src.domains.tor_net_fetcher.models import TorVsWebCountryStats
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import SQLAlchemyError
 from src.domains.shared.models import Country
+from src.domains.tor_net_fetcher.helper import HUMAN_BASELINES
+
+
 class TorNetFetchService:
     # Configuration parameters
     avg_request_size_kb = 30.0  # Average HTTP request size in KB
     http_traffic_ratio = 0.7    # 70% of traffic is HTTP/HTTPS
     protocol_overhead_ratio = 0.85  # 85% efficiency after Tor overhead
     
-    def process_response_body(self, response, country_code):
+    def process_response_body_for_tor(self, response, country_code):
         """Process relay data and calculate total requests."""
         relays = response.get('relays', [])
         sum_of_request = 0
@@ -45,7 +48,7 @@ class TorNetFetchService:
             "relay_count": len(relays)
         }
     
-    def save_record_to_db(self, db_conn: Session, total_requests: float, country_code: str):
+    def save_record_to_db(self, db_conn: Session, total_requests: float, country_code: str, tor_traffic: bool):
         """Save statistics to database."""
         try:
             print(f"Saving to database: {total_requests:,.0f} requests for {country_code.upper()}")
@@ -53,8 +56,8 @@ class TorNetFetchService:
             # Create record
             record = TorVsWebCountryStats(
                 country_code=country_code.upper(),
-                tor_traffic=True,
-                internet_traffic=False,
+                tor_traffic=tor_traffic,
+                internet_traffic= not(tor_traffic),
                 avg_http_request=int(total_requests)  # Convert to integer
             )
             
@@ -73,6 +76,8 @@ class TorNetFetchService:
             print(f"❌ Unexpected error: {e}")
             db_conn.rollback()
             return False
+        
+
     
     def fetch_live_tor_stats(self, country_code):
         """Fetch live Tor statistics and save to database."""
@@ -89,8 +94,8 @@ class TorNetFetchService:
             data = response.json()
             
             # Process data
-            results = self.process_response_body(data, country_code)
-            
+            results = self.process_response_body_for_tor(data, country_code)
+            is_tor_traffic = True
             # Save to database
             if results["total_requests"] > 0:
                 db = next(get_db())
@@ -98,7 +103,9 @@ class TorNetFetchService:
                     success = self.save_record_to_db(
                         db, 
                         results["total_requests"], 
-                        results["country"]
+                        results["country"],
+                        is_tor_traffic
+                        
                     )
                     
                     if not(success):
@@ -133,24 +140,77 @@ class TorNetFetchService:
         for country_code, in all_codes:
             result.append(country_code)
         return result
+    
+
+    def process_response_body_for_internet(self, data, country_code):
+        total_estimated_requests = HUMAN_BASELINES[country_code]
+        series = data.get('result', {}).get('serie_0', {})
+        values = [float(v) for v in series.get('values')]
+        avrg = sum(values)/len(values)
+        print(f"avg is {avrg} and total_estimated req is {total_estimated_requests}, total_req per day is {total_estimated_requests* avrg}")
+        return {"total_requests" : total_estimated_requests* avrg, "country" : country_code}
+
+
+    def fetch_live_net_stats(self, country_code):
+        """Fetch live Tor statistics and save to database."""
+        try:
+            # Build URL for specific country
+            url = f"https://api.cloudflare.com/client/v4/radar/http/timeseries?aggInterval=1h&dateRange=1d&location={country_code.upper()}&format=json"
+            cloudflare_auth_headers = { 'X-Auth-Email': 'shireenbanu89@gmail.com',
+                        'X-Auth-Key': '5504c8edcbf9d41817fbae78e9b98fa30216d'
+                      }
+            
+            print(f"Fetching Daily Internet data for {country_code.upper()}...")
+            
+            # Fetch data
+            response = requests.get(url, headers= cloudflare_auth_headers)
+            response.raise_for_status()  # Raise exception for HTTP errors
+            
+            data = response.json()
+            print(data)
+            
+            # Process data
+            results = self.process_response_body_for_internet(data, country_code)
+            print(f"results[total_requests] =  {results["total_requests"]}")
+            is_tor_traffic = False
+            # # Save to database
+            if  results["total_requests"] > 0:
+                db = next(get_db())
+                try:
+                    success = self.save_record_to_db(
+                        db, 
+                        results["total_requests"], 
+                        results["country"],
+                        is_tor_traffic
+                    )
+                    
+                    if not(success):
+                        print("❌ Failed to save to database.")
+                        
+                finally:
+                    db.close()  # Always close the database connection
+            else:
+                print("⚠️  No relays found or zero requests calculated.")
+            
+            return results
+            
+        except requests.RequestException as e:
+            print(f"❌ Network error: {e}")
+            return None
+        except json.JSONDecodeError as e:
+            print(f"❌ JSON parsing error: {e}")
+            return None
+        except Exception as e:
+            print(f"❌ Unexpected error: {e}")
+            return None
         
-
-
-
 
 if __name__ == "__main__":
     service = TorNetFetchService()
     
     # Fetch and save German Tor statistics
     country_codes = service.get_all_country_codes_from_db()
+    # country_codes = ["US"]
     for country_code in country_codes:
-        results = service.fetch_live_tor_stats(country_code)
-    
-    
-    if results:
-        print(f"\n🎯 Summary:")
-        print(f"Country: {results['country'].upper()}")
-        print(f"Relays processed: {results['relay_count']}")
-        print(f"Total daily requests: {results['total_requests']:,.0f}")
-    else:
-        print("❌ Failed to fetch or process data.")
+        service.fetch_live_net_stats(country_code)
+        service.fetch_live_tor_stats(country_code)
